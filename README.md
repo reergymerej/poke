70e4be4b334256d522820fde6f48feca3c3d0396
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-07-14 08:08:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# poke
+
+checks a url repeatedly until it returns the desired status
+
+(all hard-coded ATM)
+
+
+
+
+
+
+
+---
+kickstarted by [npm-boom][npm-boom]
+
+[npm-boom]: https://github.com/reergymerej/npm-boom
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+const url = 'https://classflowdev2.prometheanjira.com/classflow';
+const intervalMS = 20000;
+
+function isDesiredStatus(status) {
+    return status !== 403;
+}
+
+function check() {
+    console.log(`[${new Date()}] checking...`);
+    fetch(url).then(
+        resp => {
+            const {status} = resp;
+            if (!isDesiredStatus(status)) {
+                setTimeout(() => {
+                    check();
+                }, intervalMS);
+            } else {
+                console.log(`[${new Date()}] it's ready`);
+            }
+        }
+    )
+}
+
+check();
